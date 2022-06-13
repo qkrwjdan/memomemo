dc2f6f25c8fe721ed02bb1d0e9fbe8855a735869
qkrwjdan <madogisa12@naver.com>
2022-06-13 20:53:09 +0900

    15강 메모쓰기 페이지 만들기

--- lib/screens/edit.dart
+++ lib/screens/edit.dart
@@ -6,24 +6,27 @@ class EditPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          IconButton(onPressed: (){}, icon: const Icon(Icons.delete)),
-          IconButton(onPressed: (){}, icon: const Icon(Icons.save))
-        ]
+      appBar: AppBar(actions: <Widget>[
+        IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.save))
+      ]),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(hintText: '제목을 적어주세요')),
+            Padding(padding: EdgeInsets.all(10)),
+            TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(hintText: '메모의 내용을 적어주세요')),
+          ],
+        ),
       ),
-      body: Column(
-        children: <Widget>[
-          TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: '제목을 적어주세요'
-          )
-          ),
-          Padding(padding: EdgeInsets.all(10)),
-          TextField(),
-        ],
-      )
     );
   }
 }
